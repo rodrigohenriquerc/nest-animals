baaf4a2f2483baa8d6cb6b3b98c42c50cef637de
Rodrigo Carvalho <rhrc09@gmail.com>
2023-07-09 17:38:23 -0300

    feat: add validation schema

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,6 +1,20 @@
+import { IsInt, IsString } from 'class-validator';
+import * as Joi from 'joi';
+
+export const createCatSchema = Joi.object({
+  name: Joi.string().required(),
+  age: Joi.number().required(),
+  breed: Joi.string().required(),
+});
+
 export class CreateCatDto {
+  @IsString()
   name: string;
+
+  @IsInt()
   age: number;
+
+  @IsString()
   breed: string;
 }
 
